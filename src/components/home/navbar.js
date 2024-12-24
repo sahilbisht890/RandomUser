ca2bc0a356341e85d639463d1248acbe61a78293
sahilbisht <bishtrock890@gmail.com>
2024-12-24 18:01:26 +0530

    update in navbar

--- src/components/home/navbar.js
+++ src/components/home/navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 bg-slate-200">
+      <nav className="border-gray-200 dark:bg-gray-900 bg-slate-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="#"
